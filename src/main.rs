5bf499e0e1de3fc81ceec3ecd8ca9e28e15a74e7
Christopher Huntwork <christopher@huntwork.net>
2023-11-13 20:35:31 -0800

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::{port::{mode::Output, Pin}, hal::port::PB5};
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -11,9 +12,13 @@ fn main() -> ! {
     let mut led = pins.d13.into_output();
 
     loop {
-        led.set_high();
-        arduino_hal::delay_ms(1000);
-        led.set_low();
-        arduino_hal::delay_ms(1000);
+        blink(&mut led);
     }
+}
+
+fn blink(led: &mut Pin<Output, PB5>) {
+    led.set_high();
+    arduino_hal::delay_ms(1000);
+    led.set_low();
+    arduino_hal::delay_ms(1000);
 }
