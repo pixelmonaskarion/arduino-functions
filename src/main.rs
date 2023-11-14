b4ae5f1a368a21a0c690ebcde4b9cd5db5796e15
Christopher Huntwork <christopher@huntwork.net>
2023-11-13 20:45:25 -0800

    finished

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,13 @@ fn main() -> ! {
     let mut led = pins.d13.into_output();
 
     loop {
-        blink(&mut led);
+        blink(&mut led, 1000);
     }
 }
 
-fn blink(led: &mut Pin<Output, PB5>) {
-    led.set_high();
-    arduino_hal::delay_ms(1000);
+fn blink(led: &mut Pin<Output, PB5>, delay: u16) {
     led.set_low();
-    arduino_hal::delay_ms(1000);
+    arduino_hal::delay_ms(delay);
+    led.set_high();
+    arduino_hal::delay_ms(delay);
 }
